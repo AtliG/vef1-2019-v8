a75f18029d3345c0920e2ca14f5c51d7c05afb5a
Atli <atligudjonsson6@gmail.com>
2019-11-06 22:55:12 +0000

    Add og DeleteItem

--- scripts.js
+++ scripts.js
@@ -14,20 +14,37 @@ const text = (() => {
     items = _items;
     _form.addEventListener('submit', formHandler);
 
+    inputArea = _form.querySelector('.form__input');
+
+    const tasksArray = items.getElementsByTagName("li");
+    
+    for(let i = 0; i < tasksArray.length; i++) {
+      const item = tasksArray[i].children[2].parentNode;
+      tasksArray[i].children[0].addEventListener('click', () => {
+        finish(tasksArray[i]);
+      });
+      tasksArray[i].children[1].addEventListener('click', () => {
+        edit(tasksArray[i].children[1]);
+      });
+      tasksArray[i].children[2].addEventListener('click', () => {
+        deleteItem(item);
+      });
+    }
     // TODO láta hluti í _items virka
   }
 
   function formHandler(e) {
     e.preventDefault();
-
-    console.log('halló heimur');
+    console.log('jello');
+    add(inputArea.value);
   }
 
   // event handler fyrir það að klára færslu
   function finish(e) {
+    e.classList.toggle('item--done');
   }
 
-  // event handler fyrir það að breyta færslu
+  // event handler fyrir það að breyta færslu 
   function edit(e) {
   }
 
@@ -37,14 +54,41 @@ const text = (() => {
 
   // fall sem sér um að bæta við nýju item
   function add(value) {
+    if(inputArea !== null) {
+      const listItem = el('li','item', null);
+      const checkBox = el('input', 'item__checkbox', () => {
+        finish(listItem);
+      });
+      checkBox.setAttribute('type', 'checkbox');
+      const span = el('span', 'item__text', () => {
+        edit(listItem);
+      });
+      span.appendChild(document.createTextNode(value));
+      const button = el('button', 'item__button', () => {
+        deleteItem(listItem);
+      });
+      button.appendChild(document.createTextNode('Eyða'));
+      listItem.appendChild(checkBox);
+      listItem.appendChild(span);
+      listItem.appendChild(button);
+
+      items.appendChild(listItem);
+
+      input.value('');
+    }
   }
 
   // event handler til að eyða færslu
   function deleteItem(e) {
+    e.remove();
   }
 
   // hjálparfall til að útbúa element
   function el(type, className, clickHandler) {
+    const elem = document.createElement(type);
+    elem.classList.add(className);
+    elem.addEventListener('click', clickHandler);
+    return elem;
   }
 
   return {
